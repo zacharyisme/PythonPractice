dba407f3d6f23b81ce47c5335bb249b76770654a
Zachary Huang <zacharyisme@gmail.com>
2015-04-28 08:45:14 +0800

    Add html_generator for string and list
    html_generator.py for String
    html_generator_list.py for list

---
+++ String/html_generator.py
@@ -0,0 +1,56 @@
+def generate_concept_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def get_title(concept):
+    start_location = concept.find('TITLE:')
+    end_location = concept.find('DESCRIPTION:')
+    title = concept[start_location+7 : end_location-1]
+    return title
+
+def get_description(concept):
+    start_location = concept.find('DESCRIPTION:')
+    description = concept[start_location+13 :]
+    return description
+
+def get_concept_by_number(text, concept_number):
+    counter = 0
+    while counter < concept_number:
+        counter = counter + 1
+        next_concept_start = text.find('TITLE:')
+        next_concept_end   = text.find('TITLE:', next_concept_start + 1)
+        if next_concept_end >= 0:
+            concept = text[next_concept_start:next_concept_end]
+        else:
+            next_concept_end = len(text)
+            concept = text[next_concept_start:]
+        text = text[next_concept_end:]
+    return concept
+
+def generate_all_html(text):
+    current_concept_number = 1
+    concept = get_concept_by_number(text, current_concept_number)
+    all_html = ''
+    while concept != '':
+        title = get_title(concept)
+        description = get_description(concept)
+        concept_html = generate_concept_HTML(title, description)
+        all_html = all_html + concept_html
+        current_concept_number = current_concept_number + 1
+        concept = get_concept_by_number(text, current_concept_number)
+    return all_html
+
+
+print generate_all_html(TEST_TEXT)
